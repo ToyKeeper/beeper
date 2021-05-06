e0bd3d0a27272d2c43b93d52c5e0d7c1e4b45c52
Selene ToyKeeper <git@toykeeper.net>
2021-05-06 12:00:52 -0600

    test / example improvements... - added -v to show details of each test (or not) - added a bunch of test patterns - converted frequencies to note names where appropriate

--- tests.sh
+++ tests.sh
@@ -1,62 +1,91 @@
 #!/bin/sh
 
+if [ x"$1" = x"-v" ]; then
+  VERBOSE=1
+fi
+
 B=./beeper.py
 
 main () {
   style=square
+
+  echo kick-1
+  b 8000 -w tri -l 5 -v 100 20 -w sq -l 1 -v 200 -d 1
+  echo kick-2
+  b 12000 -w sq -l 5 -v 100 20 -w sq -l 1 -v 200 -d 1
+  echo kick-3
+  b 8000 -v 100 -w tri -l 0.3 18870 15725 13104 10920 9100 7583 6319 5266 4388 3657 3047 2539 2116 1763 1469 1224 1020 850 708 590 492 410 341 284 237 197 164 137 114 95 79 66 55 46 38 31 -l 2.2 -d 0.5
+
+  echo tri-square-saw
   b -l 750 -n C4 -w tri -n C4 -w square -n C4 -w saw
 
   for style in square saw tri ; do
+    echo "=== $style ==="
     echo volume test
-    b -l 500 -n C5 -v 1 -n C5 -v 10 -n C5 -v 100 -n C5 -v 500
+    b -l 333 C5 -v 1 C5 -v 3 C5 -v 10 C5 -v 33 C5 -v 100 C5 -v 200 C5 -v 500
     echo AC power plugged in
     b -f 880 -l 50 -f 1200 -l 50
     echo AC power unplugged
     b -f 880 -l 50 -f 600 -l 50
+    echo on-battery-power
+    b -l 30 2000 500
     echo 1up
-    b -l 50 -f 200 -f 400 -f 600 -f 800 -f 1000 -f 1200
+    b -l 50 -f 200 -f 400 -f 600 -f 800 -f 1000 -f 1200 -f 1600
     echo minor-lo
-    b -l 50 -f 500 -D 20 -f 600 -D 20 -f 750
+    b -l 50 -n B4 -D 20 -n D5 -D 20 -n F+5
     echo minor-hi
-    b -l 50 -f 1308 -D 20 -f 1555 -D 20 -f 1959
+    b -l 50 -n E6 -D 20 -n G6 -D 20 -n B6
     echo minor-hi-back
-    b -l 30 -f 1308 -D 10 -f 1555 -D 10 -f 1959 -D 10 -f 1555 -D 10 -f 1308
+    b -l 40 E6 G6 B6 G6 E6
     echo minor-hi-2
-    b -l 30 -f 1100 -D 10 -f 1308 -D 10 -f 1648 -D 10 -f 1100 -D 10 -f 1308 -D 10 -f 1648
+    b -l 30 -n C+6 -D 10 -n E6 -D 10 -n G+6 -D 10 -n C+6 -D 10 -n E6 -D 10 -n G+6
+    b -l 40 C+6 E6 G+6 C+6 E6 G+6
     echo minor-arpeggio
-    b -l 12 -f 1100 -f 1308 -f 1648 -f 1100 -f 1308 -f 1648 -f 1100 -f 1308 -f 1648 -f 1100 -f 1308 -f 1648
+    b -l 12 C+6 E6 G+6  C+6 E6 G+6  C+6 E6 G+6  C+6 E6 G+6
     echo major-lo
-    b -l 50 -f 500 -D 20 -f 625 -D 20 -f 750
+    b -l 50 B4 -d 20 D+5 -d 20 F+5
     echo major-hi
-    b -l 50 -f 1308 -D 20 -f 1648 -D 20 -f 1959
+    b -l 50 E6 -D 20 G+6 -D 20 B6
     echo major-hi-back
-    b -l 30 -f 1308 -D 10 -f 1648 -D 10 -f 1959 -D 10 -f 1648 -D 10 -f 1308
+    b -l 30 E6 -D 10 G+6 -D 10 B6 -D 10 G+6 -D 10 E6
     echo slip-down
-    b -l 20 -f 1308 -D 5 -f 1234 -D 5 -f 1165 -D 5 -f 1100 -D 5 -f 1038
+    b -l 20 E6 -D 5 D+6 -D 5 D6 -D 5 C+6 -D 5 C6
     echo slip-down-short
-    b -l 20 -f 1308 -D 5 -f 1234 -D 5 -f 1165
+    b -l 20 E6 -D 5 D+6 -D 5 D6
     echo slip-up
-    b -l 20 -f 1234 -D 5 -f 1308 -D 5 -f 1385 -D 5 -f 1468 -D 5 -f 1555 -D 5 -f 1648
+    b -l 20 D+6 -D 5 E6 -D 5 F6 -D 5 F+6 -D 5 G6 -D 5 G+6
     echo slip-up-short
-    b -l 20 -f 1234 -D 5 -f 1308 -D 5 -f 1385
+    b -l 20 D+6 -D 5 E6 -D 5 F6
     echo random1
     b -l 30 -f 1527 -D 5 -f 2153 -D 5 -f 1721 -D 5 -f 1254 -D 5 -f 2153
     echo slide-up
-    b -l 7 -f 327 -f 367 -f 412 -f 462 -f 550 -f 654 -f 734 -f 824 -f 924 -f 1038 -f 1165 -f 1234 -f 1308 -f 1385
+    b -l 7 E4 F+4 G+4 A+4 C+5 E5 F+5 G+5 A+5 C6 D6 D+6 E6 F6
+    echo red alert
+    b -l 1 $(seq 100 1000)
+    echo laser
+    b -l 0.5 $(seq 1000 -1 100)
+    echo ufo
+    #b -l 0.5 $(seq 4000 -2 2000) $(seq 2000 2 4000) $(seq 4000 -2 2000) $(seq 2000 2 4000)
+    b -l 1 $(seq 4000 -5 2000) $(seq 2000 5 4000) $(seq 4000 -5 2000) $(seq 2000 5 4000)
+    echo pacman
+    b -l 0.25 $(seq 4000 -5 2000) $(seq 2000 5 4000) $(seq 4000 -5 2000) $(seq 2000 5 4000)
     echo big blues scale
     b -l 100 \
-      -n C1 -n D+1 -n F1 -n F+1 -n G1 -n A+1 \
-      -n C2 -n D+2 -n F2 -n F+2 -n G2 -n A+2 \
-      -n C3 -n D+3 -n F3 -n F+3 -n G3 -n A+3 \
-      -n C4 -n D+4 -n F4 -n F+4 -n G4 -n A+4 \
-      -n C5 -n D+5 -n F5 -n F+5 -n G5 -n A+5 \
-      -n C6 -n D+6 -n F6 -n F+6 -n G6 -n A+6 \
-      -n C7 -n D+7 -n F7 -n F+7 -n G7 -n A+7 -n C8
+      C1 D+1 F1 F+1 G1 A+1 \
+      C2 D+2 F2 F+2 G2 A+2 \
+      C3 D+3 F3 F+3 G3 A+3 \
+      C4 D+4 F4 F+4 G4 A+4 \
+      C5 D+5 F5 F+5 G5 A+5 \
+      C6 D+6 F6 F+6 G6 A+6 \
+      C7 D+7 F7 F+7 G7 A+7 C8
   done
+
 }
 
 b () {
-  echo "$B -w $style $*"
+  if [ ! -z "$VERBOSE" ]; then
+    echo "$B -w $style $*"
+  fi
   $B -w $style $*
   sleep 0.5
 }
